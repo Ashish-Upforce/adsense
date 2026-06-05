b7065b9e722178e8e7f5e029d916eb6d14a09502
Ashish Rathod <ashish.rathod@upfroce.tech>
2026-06-05 14:11:20 +0530

    Multi

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
     crossOrigin="anonymous"
     strategy="afterInteractive"
   />
+  <Script src="https://quge5.com/88/tag.min.js" data-zone="246394" async data-cfasync="false"></Script>
 <Script
   id="nap5k-script"
   strategy="afterInteractive"
